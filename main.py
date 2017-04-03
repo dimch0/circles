ce853a772418a1e63842991167db92b631fff379
Dimcho <dimcho.dzhartov@strypes.eu>
2017-04-03 12:20:55 +0300

    main_created

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ grey = (195, 195, 195)
 pink = (252, 217, 229)
 green = (210, 255, 191)
 
+
 FPS = 30
-body_step = 1
-fi = 30
+SCALE = 4
+
+fi = 30 / SCALE
+display_width = 600 / SCALE
+display_height = 600 / SCALE
 
 
-display_width = 800
-display_height = 600
-
-mid_x = int((display_width-200)/2)
+mid_x = int(display_width/2)
 mid_y = int(display_height/2)
 
 
@@ -27,6 +28,35 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('radius')
 
 clock = pygame.time.Clock()
+
+
+
+class Grid(object):
+    """
+    master class for the grid
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def gen_dict(fi):
+        result = []
+        katet = int(sqrt(((2 * fi) ** 2) - (fi ** 2)))
+        for x in range(0, 12):
+            for y in range(1, 20):
+                if x % 2 == y % 2:
+                    centre_x = fi + (x * katet)
+                    centre_y = y * fi
+                    centre = [centre_x, centre_y]
+                    result.append(centre)
+                    # pygame.draw.circle(gameDisplay, white, centre, fi, 1)
+        return result
+
+
+grid = Grid.gen_dict(fi)
+print grid
+
 
 def Tracks(point_A, Point_B, speed = 5):
     track = []
@@ -43,24 +73,20 @@ def Tracks(point_A, Point_B, speed = 5):
         for i in range(steps_number+1):
             step = [int(ax + stepx * i), int(ay + stepy * i)]
             track.append(step)
-    # track.append(Point_B)
+    track.append(Point_B)
     return track
 
 
 def game_loop():
 
     game_exit = False
-
-    lead_x = 336
+    lead_x = mid_x
     lead_y = mid_y
-
     menu = False
-
     END_POINT = None
     COUNT = 0
 
     while not game_exit:
-
         # MOUSE POSITION X AND Y
         mouse_pos = pygame.mouse.get_pos()
         mx = mouse_pos[0]
@@ -69,7 +95,6 @@ def game_loop():
         for event in pygame.event.get():
             # print event
             if event.type == pygame.QUIT:
-
                 game_exit = True
 
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -81,14 +106,12 @@ def game_loop():
                 COUNT = 0
                 print "COUNT", COUNT
 
-
                 if in_circle(lead_x, lead_y, fi, mx, my) and not menu:
                     menu = True
                     print menu
                 else:
                     menu = False
                     print menu
-
 
         if END_POINT:
             #print "COUNTTTT", COUNT
@@ -98,16 +121,12 @@ def game_loop():
             lead_y = track[COUNT][1]
 
         # BACKGROUND
-        gameDisplay.fill(black)
-
+        gameDisplay.fill(grey)
 
         # TODO blit stuff
-
         # GRID
-        for x in range(0, 12):
-            for y in range(1, 20):
-                if x % 2 == y % 2:
-                    pygame.draw.circle(gameDisplay, white, [fi + (x * 51), y * fi], fi, 1)
+        for centre in grid:
+            pygame.draw.circle(gameDisplay, white, centre, fi, 1)
 
         if menu:
             pygame.draw.circle(gameDisplay, green, [lead_x, lead_y], fi * 3, 0)
@@ -115,11 +134,9 @@ def game_loop():
         # body
         pygame.draw.circle(gameDisplay, pink, (lead_x, lead_y), fi, 0)
         # body range:
-        pygame.draw.circle(gameDisplay, white, (lead_x, lead_y), 300, 1)
-
+        pygame.draw.circle(gameDisplay, white, (lead_x, lead_y), fi * 10, 1)
 
         pygame.display.update()
-
 
         # TODO: if change vars to blit
         clock.tick(FPS)
@@ -133,8 +150,6 @@ def in_circle(center_x, center_y, radius, x, y):
     return square_dist <= radius ** 2
 
 
-game_loop()
-
-
-asdasd
+if __name__ == '__main__':
+    game_loop()
 
