c0215a49409f38e1d3e697faf1c5ec35a43c325c
Dimcho <dimcho.dzhartov@strypes.eu>
2017-10-18 14:55:23 +0300

    new galab img

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ def game_loop(game_over, scenario="scenario_1"):
     grid               = Grid(pygame, scenario)
     grid.loader        = DataLoader(grid)
     my_body            = grid.loader.load_game()
+    my_body.img = grid.images.galab3
 
     if game_over:
         grid.rename_button("play", "replay")
